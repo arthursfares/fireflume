2734a42ac9f83d993f8307793028b8ac36d874a6
arthursfares <arthursfares@gmail.com>
2023-08-03 05:02:52 -0300

    add signup page

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 
-from login_page import show_login_page
+from login_page import show_auth_page
 from main_page import show_main_page
 
 def main():
     st.title("Flume")
     if "logged" not in st.session_state:
         st.session_state["logged"] = False
+        st.session_state["signing"] = False
     if st.session_state["logged"]:
         show_main_page()
     else:
-        show_login_page()
+        show_auth_page()
 
 if __name__ == "__main__":
     main()
--- login_page.py
+++ login_page.py
@@ -12,8 +12,30 @@ def _login_callback(email, password):
         st.session_state["logged"] = False
         st.error("Invalid email or password")
 
-def show_login_page():
-    with st.container():
-        email = st.text_input(label="Email", placeholder="email")
-        password = st.text_input(label="Password", placeholder="password", type="password")
-        st.button("Log In", on_click=_login_callback, args=(email, password))
+def _goto_signup_callback():
+    st.session_state["signing"] = True
+
+def _show_login_section():
+    email = st.text_input(label="Email", placeholder="email")
+    password = st.text_input(label="Password", placeholder="password", type="password")
+    st.button("Log In", on_click=_login_callback, args=(email, password))
+    st.button("Go to sign up", on_click=_goto_signup_callback)
+
+
+
+def _signup_callback():
+    st.session_state["signing"] = False
+
+def _show_signup_section():
+    username = st.text_input(label="Username", placeholder="username")
+    email = st.text_input(label="Email", placeholder="email")
+    password = st.text_input(label="Password", placeholder="password", type="password")
+    st.button("Sign In", on_click=_signup_callback)
+
+
+
+def show_auth_page():
+    if not st.session_state["signing"]:
+        _show_login_section()
+    else:
+        _show_signup_section()
