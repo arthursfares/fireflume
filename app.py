5209bfd5de6a1df9f656b96e088bc0eaa8ad4d3d
arthursfares <arthursfares@gmail.com>
2023-08-03 12:10:46 -0300

    center title

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import streamlit as st
+from streamlit_extras.row import row
 
 from auth_page import show_auth_page
 from main_page import show_main_page
 
 def main():
-    st.title("Flume")
+    title_html = "<h1 style='text-align: center;'>Flume</h1>"
+    st.markdown(title_html, unsafe_allow_html=True)
     if "logged" not in st.session_state:
         st.session_state["logged"] = False
         st.session_state["signing"] = False
