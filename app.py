29d1ef007625130450aab77e35e99cfd5c97f6f4
arthursfares <arthursfares@gmail.com>
2023-08-08 13:53:40 -0300

    update title

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ from routes.auth_page import show_auth_page
 from routes.main_page import show_main_page
 
 def main():
-    title_html = "<h1 style='text-align: center;'>FireFlume</h1>"
-    st.markdown(title_html, unsafe_allow_html=True)
-    title_html = "<h3 style='text-align: center;'>streamlit firebase integration</h3>"
-    st.markdown(title_html, unsafe_allow_html=True)
     if "logged" not in st.session_state:
         st.session_state["logged"] = False
         st.session_state["signing"] = False
+    if not st.session_state["logged"]:
+        title_html = "<p style='text-align: center;'> <img src='https://media.tenor.com/p3JESU9FOfoAAAAi/this-is-fine-fire.gif' width='100'> </p>"
+        st.markdown(title_html, unsafe_allow_html=True)
+        title_html = "<h3 style='text-align: center;'>fireflume</h3>"
+        st.markdown(title_html, unsafe_allow_html=True)
     if st.session_state["logged"]:
         show_main_page()
     else:
