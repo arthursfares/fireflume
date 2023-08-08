e6c076fb379704751fc9688de447a5ff5ca96482
arthursfares <arthursfares@gmail.com>
2023-08-08 13:35:23 -0300

    add alternative config initialization example

--- firebase/auth.py
+++ firebase/auth.py
@@ -1,5 +1,6 @@
 import pyrebase
 import streamlit as st
+# from firebase_config import firebase_config
 
 class Singleton(type):
     _instances = {}
@@ -10,11 +11,17 @@ class Singleton(type):
 
 class AuthFirebase(metaclass=Singleton):
     def __init__(self):
+        ''' for config values stored in streamlit secrets '''
         config = {}
         for key in st.secrets:
             config[str(key)] = st.secrets[str(key)]
         firebase = pyrebase.initialize_app(config)
+        ''' for config values stored in python firebase_config.py file '''
+        # firebase = pyrebase.initialize_app(firebase_config)
+        ''' services initialization '''
         self.auth = firebase.auth()
+        # self.storage = firebase.storage()
+        # self.database = firebase.database()
 
     def sign_in(self, email, password):
         response = self.auth.sign_in_with_email_and_password(email, password)
